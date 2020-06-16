d605288029535abb79eaf7e7b277afb659b0eca1
Jack Skiles <jackskiles420@gmail.com>
2020-06-16 10:21:12 -0400

    updated functionality

---
+++ src/components/IssueList.jsx
@@ -0,0 +1,38 @@
+  
+import React, { Component } from 'react'
+import Issue from './Issue';
+
+import './IssuesList.css';
+
+class IssueList extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            issues: []
+        }
+    }
+    getIssue = () => {
+        console.log('Mounted the profile');
+        fetch('https://api.github.com/repos/facebook/create-react-app/issues')
+        .then(res => res.json())
+        .then(data => {
+            this.setState({ issues: data ? data : []})
+            console.log(this.state.issues);
+        })
+    }
+    componentDidMount() {
+        this.getIssue();
+    }
+    render() {
+        return (
+            <div>
+                <div className="IssuesList">
+                    {this.state.issues.map(issue => <Issue issue={issue} key={issue.id } />)}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default IssueList;
